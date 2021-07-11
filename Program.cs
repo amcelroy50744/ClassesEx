18f6bc0504179776f66171fcf44b26562db4a66f
Andre McElroy <amcelroy50744@gmail.com>
2021-07-11 13:45:07 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Ford";
+           myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carList = new List<Car>() { myCar, mazda, chevy };
+            foreach(var vehicle in carList) 
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+        }
+    }
+}
