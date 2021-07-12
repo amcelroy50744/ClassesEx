214da6308a4cff31e7dfc05af1b85cd4d3e56a67
amcelroy50744 <85271297+amcelroy50744@users.noreply.github.com>
2021-07-12 09:52:15 -0400

    Add files via upload

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+   public class Car
+    {
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; } 
+
+
+        public Car() 
+        {
+        
+        }
+
+       public void MakeEngineNoise(string engineNoise) 
+        {
+            Console.WriteLine(engineNoise);
+        }
+
+        public void MakeHonkNoise(string honkNoise) 
+        {
+            Console.WriteLine(honkNoise);
+        }
+    }
+}
